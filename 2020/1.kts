26a1df75d2ab1daca966e437bf63266acb81e90d
akstupp <akstupp@gmail.com>
2020-12-26 21:04:19 -0500

    Restructure day 1.

--- 2020/1.kts
+++ 2020/1.kts
@@ -4,22 +4,26 @@ fun readInputFile(filename: String): List<String> {
     return File(filename).readLines()
 }
 
-fun partA(vals: List<Integer>) {
+fun partA(vals: List<Int>): Int {
     for (one in vals)
         for (two in vals)
             if (one + two == 2020)
-                println(one * two)
+                return one * two
+    return -1
 }
 
-fun partB(vals: List<Integer>) {
+fun partB(vals: List<Int>): Int {
     for (one in vals)
         for (two in vals)
             for (three in vals)
                 if (one + two + three == 2020)
-                    println(one * two * three)
+                    return one * two * three
+    return -1
 }
 
 var stringVals = readInputFile("1input")
 var vals  = stringVals.map { s -> Integer.valueOf(s) }
-partA(vals)
-partB(vals)
+val answerA = partA(vals)
+val answerB = partB(vals)
+println("Part A: " + answerA)
+println("Part B: " + answerB)
