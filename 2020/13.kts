946292343ce0485b47157149f2a6e914fafa8d3e
akstupp <akstupp@gmail.com>
2021-01-24 19:25:54 -0500

    13.2.

--- 2020/13.kts
+++ 2020/13.kts
@@ -1,26 +1,56 @@
 import java.io.File
 
-typealias Time = Long
+typealias Timestamp = Long
 typealias BusID = Int
+typealias Index = Int
 
-fun parseInput(filename: String): Pair<Time, List<BusID>> {
-    var lines = File(filename).readLines()
-    val time = lines[0].toLong()
-    val busIDs = lines[1].split(',').filter { it[0] != 'x' }.map { it.toInt() }
-    return Pair(time, busIDs)
+fun parseLines(filename: String): List<String> {
+    return File(filename).readLines()
 }
 
-fun findEarliestBus(time: Time, buses: List<BusID>): Long {
-    var candidates = mutableListOf<Pair<BusID, Time>>()
-    for (bus in buses) {
-        val min = time / bus
-        candidates.add(Pair(bus, bus * (min + 1)))
+fun parseTimestamp(filename: String): Timestamp {
+    return parseLines(filename).get(0).toLong()
+}
+
+fun parseBusSchedule(filename: String): List<Pair<Index, BusID>> {
+    return parseLines(filename)
+            .get(1)
+            .split(',')
+            .mapIndexed { index, s -> Pair(index, s) }
+            .filter {it.second[0] != 'x'}
+            .map { Pair(it.first, it.second.toInt()) }
+}
+
+fun findEarliestBus(time: Timestamp, buses: List<Pair<Index, BusID>>): Long {
+    var candidates = mutableListOf<Pair<BusID, Timestamp>>()
+    for (bus in buses.map { it.second }) {
+        val next = (time / bus) + 1
+        candidates.add(Pair(bus, bus * next))
     }
     candidates.sortBy { it.second }
     val earliest = candidates.first { it.second > time }
     return earliest.first * (earliest.second - time)
 }
 
-var (departure, buses) = parseInput("13_input")
-var calculation = findEarliestBus(departure, buses)
-println("Part 1: $calculation")
+fun findChainedBusSchedule(buses: List<Pair<Index, BusID>>): Long {
+    var d = 1L
+    var i = 0L
+    for ((offset, bus) in buses) {
+        while (true) {
+            i += d
+            if ((i + offset) % bus == 0L) {
+                d = d * bus
+                break
+            }
+        }
+    }
+    return i
+}
+
+var file = "13_input"
+var timestamp = parseTimestamp("13_input")
+var busSchedule = parseBusSchedule("13_input")
+var earliestBus = findEarliestBus(timestamp, busSchedule)
+println("Part 1: $earliestBus")
+var calculation = findChainedBusSchedule(busSchedule)
+println("Part 2: $calculation")
