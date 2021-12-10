1ebf3fe92b2f8c1ea62ba29f8cf546adcb7a3c9a
akstupp <akstupp@gmail.com>
2021-12-09 22:31:13 -0500

    Remove unnecessary function call

--- 2021/src/day01/sonar_sweep.kts
+++ 2021/src/day01/sonar_sweep.kts
@@ -1,21 +1,18 @@
 import java.io.File
 
-fun readInputFile(filename: String): List<Int> {
-    return File(filename).readLines().map { it.toInt() }
-}
-
 fun countIncrements(depths: List<Int>): Int {
     return depths.windowed(2).count { a, b -> a < b }
 }
 
 fun countIncreasingWindows(depths: List<Int>): Int {
-    val sumList = depths.windowed(3, 1).map { window -> window.sum() }
+    val sumList = depths.windowed(3, 1)
+            .map { window -> window.sum() }
     return countIncrements(sumList)
 }
 
-val input = readInputFile("data")
+val depths = File("data").readLines().map { it.toInt() }
 // Part 1
-val incrementCount = countIncrements(input)
+val incrementCount = depths.windowed(2).count { a, b -> a < b }
 println("Part One: $incrementCount")
 // Part 2
 val slidingWindowCount = countIncreasingWindows(input)
