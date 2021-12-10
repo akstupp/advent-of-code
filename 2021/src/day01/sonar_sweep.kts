c788e7f7205ecb86d29997a1a90186fb495f7006
akstupp <akstupp@gmail.com>
2021-12-09 22:32:22 -0500

    Remove unnecessary function call

--- 2021/src/day01/sonar_sweep.kts
+++ 2021/src/day01/sonar_sweep.kts
@@ -12,7 +12,7 @@ fun countIncreasingWindows(depths: List<Int>): Int {
 
 val depths = File("data").readLines().map { it.toInt() }
 // Part 1
-val incrementCount = depths.windowed(2).count { a, b -> a < b }
+val incrementCount = countIncrements(depths)
 println("Part One: $incrementCount")
 // Part 2
 val slidingWindowCount = countIncreasingWindows(input)
