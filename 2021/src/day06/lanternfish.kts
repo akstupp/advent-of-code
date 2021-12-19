6e605f32ee57d09172860b097b314c2c89ae7f69
akstupp <akstupp@gmail.com>
2021-12-19 13:33:43 -0500

    Day 6

---
+++ 2021/src/day06/lanternfish.kts
@@ -0,0 +1,47 @@
+import java.io.File
+import java.nio.charset.Charset
+
+// Returns a map of counts of each fish in the input
+fun parseInput(filename: String): Map<Int, Int> {
+    return File(filename).readText(Charset.defaultCharset())
+            .split(",")
+            .map { it.toInt() }
+            .groupingBy { it }
+            .eachCount()
+}
+
+fun spawn(iterations: Int, dayToCount: Map<Int, Int>): Long {
+    val spawnCounts: MutableMap<Int, Long> = IntRange(0, 8)
+            .associate { it to 0L }
+            .toMutableMap()
+
+    dayToCount.forEach { (day, count) ->
+        spawnCounts[day] = count.toLong()
+    }
+
+    for (i in 1..iterations) {
+        val newbornFish = spawnCounts[0]!!
+        spawnCounts.keys.forEach { fish ->
+            if (fish != 0)
+                spawnCounts[fish - 1] = spawnCounts[fish]!!
+        }
+        // Add the newborns to the countdown
+        spawnCounts[8] = newbornFish
+
+        // Reset those that have finished
+        spawnCounts.merge(6, newbornFish) { a, b -> a + b }
+    }
+    return spawnCounts.values.sum()
+}
+
+var input = parseInput("test")
+check(spawn(80, input) == 5934L)
+check(spawn(256, input) == 26984457539L)
+
+input = parseInput("data")
+
+val part1 = spawn(80, input)
+println("part 1: $part1")
+
+val part2 = spawn(256, input)
+println("Part 2: $part2")
